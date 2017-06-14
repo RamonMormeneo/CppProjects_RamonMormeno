54b441b5b6e513ea2ecd5818e690318e67bf3ea3
RamonMormeneo <Dolors@DESKTOP-PC>
2017-06-14 19:04:07 +0200

    gfgjgfjg

--- queue/Source.cpp
+++ queue/Source.cpp
@@ -1,11 +1,34 @@
-#include "thequeue.h"
+
 #include <iostream>
 #include <queue>
+#include <string>
+struct ola
+{
+	int a;
+	char c;
+	int fat = 0;
+	bool operator()(const ola&b, const ola& d)
+	{
+		return b.fat > d.fat;
+	}
+};
 
+void SelectionSort(std::queue<ola> a)
+{
+	int minIndex, tmp;
+	for (int i = 0; i < a.size() - 1; i++);
+}
 void main()
 {
-	thequeue a;
-	a.push(2);
-	a.push(3);
-	a.pop();
+	std::priority_queue <ola,std:: vector <ola>,ola> a;
+	ola b;
+	ola c;
+	ola d;
+	d.fat = 3;
+	c.fat = 1;
+	a.push(d);
+	a.push(b);
+	a.push(c);
+	int x = a.size();
+	
 }
